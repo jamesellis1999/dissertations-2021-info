aa6209ab80929e1de316558a97a4f07bf4b86883
Kasturi Mitra <98.kasturi@gmail.com>
2021-06-25 16:59:33 +0100

    script + output TC NER SpaCy updated

--- kasturi_mitra_find/techcrunch_scrape/ner_spacy_tc.py
+++ kasturi_mitra_find/techcrunch_scrape/ner_spacy_tc.py
@@ -2,10 +2,11 @@
 # use SpaCy
 import pandas as pd
 import spacy
+import time
 from itertools import *
 
 data = pd.read_csv("techcrunch_articles.csv", index_col=0)
-columns = ['people', 'orgs', 'potential_startups']
+columns = ['article_id', 'people', 'orgs', 'potential_startups']
 article_entities = pd.DataFrame(columns=columns)
 
 for i in range(0, len(data)):
@@ -17,8 +18,7 @@ for i in range(0, len(data)):
 
             body = data.iloc[i]['body']
             title = data.iloc[i]['title']
-            # print(body)
-
+            # print(title)
 
             # NER with SpaCy
             nlp = spacy.load("en_core_web_sm")
@@ -32,15 +32,26 @@ for i in range(0, len(data)):
             except: # no key error
                 orgs = []
             # print(orgs)
+
+            # remove possesive apostrophes 's and ’s (two distinct unicode characters)
+            title = title.replace("'s", "")
+            title = title.replace("’s", "")
+            # for words ending with 's'
+            title = title.replace("s'", "s")
+            title = title.replace("s’", "s")
+            for org in range(len(orgs)):  # replace in place
+                orgs[org] = orgs[org].replace("'s", "")
+                orgs[org] = orgs[org].replace("’s", "")
+                orgs[org] = orgs[org].replace("s'", "s")
+                orgs[org] = orgs[org].replace("s’", "s")
+
             potential_startups = []
-            title = title.replace("'s", "") # remove possesive apostrophes
+            # if an ORG in body is present in the title, it is a potential startup
             for org in orgs:
                 if title.find(org) != -1:
                     potential_startups.append(org)
             if len(potential_startups) == 0: # no matching ORG in body and title
                 potential_startups = orgs
-
-            # potential_founders = entities['PERSON']
 
             people = None
             try:
@@ -49,7 +60,7 @@ for i in range(0, len(data)):
                 people = []
             potential_founders = people
 
-            temp = pd.DataFrame([[potential_founders, orgs, potential_startups]], columns=columns)
+            temp = pd.DataFrame([[i, potential_founders, orgs, potential_startups]], columns=columns)
             article_entities = pd.concat([article_entities, temp], ignore_index=True)
 
             print("")
@@ -61,9 +72,15 @@ for i in range(0, len(data)):
             print(e)
             pass
 
-
-
-
-
 # save locally
-article_entities.to_csv("articles_entities_spacy.csv", encoding='utf-8-sig')
+while True:
+    try:
+        time.sleep(5)
+        article_entities.to_csv("article_entities_spacy.csv", encoding='utf-8-sig')
+        print("CSV stored!")
+        break
+    except KeyboardInterrupt:
+        print("Trying again in 20s...")
+    except:
+        print("Error storing file...")
+        pass
