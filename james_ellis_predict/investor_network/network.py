fd11955c497de714c63c4a655f9229e8c36cb146
James Ellis <jamesellis9570@yahoo.co.uk>
2021-07-14 13:14:41 +0100

    eda added

--- james_ellis_predict/investor_network/network.py
+++ james_ellis_predict/investor_network/network.py
@@ -21,7 +21,7 @@ def centrality_t(datestring, metric = 'closeness'):
 
     data = investments(datestring)
 
-    invest_type = data[['lead_investor_uuids', 'most_common_invest_type']].drop_duplicates(subset='lead_investor_uuids')
+    invest_type = data[['lead_investor_uuids', 'most_common_invest_type']].drop_duplicates(subset='lead_investor_uuids').rename(columns={'lead_investor_uuids':'uuid'})
 
     G = nx.from_pandas_edgelist(data, 'org_uuid', 'lead_investor_uuids')
 
@@ -31,11 +31,11 @@ def centrality_t(datestring, metric = 'closeness'):
     c_vals = metrics[metric](P)
     
     df = pd.DataFrame(c_vals.items(), columns = ['uuid', '{}_centrality'.format(metric)])
-    investors = pd.read_csv('../../../data/raw/investors.csv', usecols=['uuid', 'name', 'investor_types', 'investment_count', 'total_funding_usd'])
+    investors = pd.read_csv('../../../data/raw/investors.csv', usecols=['uuid', 'name', 'investor_types'])
     
     merge = pd.merge(df, investors, on='uuid', how='left')
-    merge = pd.merge(merge, invest_type, left_on='uuid', right_on='lead_investor_uuids', how='left')
-    tabloo.show(merge)
+    merge = pd.merge(merge, invest_type, on='uuid', how='left')
+    merge.to_csv('vc_centrality.csv', index=False)
 
     
 if __name__=="__main__":
